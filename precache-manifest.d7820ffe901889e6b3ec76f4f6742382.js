8566a000278be8d0c5eb2aea6e3999eab36aef99
SourishGhosh <sourish.njs@gmail.com>
2020-09-16 00:06:01 +0530

    Updates

---
+++ precache-manifest.d7820ffe901889e6b3ec76f4f6742382.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e5b0d7589502e8fc04546e8abee1eec",
+    "url": "/task-mcm/index.html"
+  },
+  {
+    "revision": "d6211c8da81b0453f245",
+    "url": "/task-mcm/static/css/main.5fe4566f.chunk.css"
+  },
+  {
+    "revision": "8b2e156c3b72fd4c57ea",
+    "url": "/task-mcm/static/js/2.310a559f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/task-mcm/static/js/2.310a559f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6211c8da81b0453f245",
+    "url": "/task-mcm/static/js/main.48ea9a14.chunk.js"
+  },
+  {
+    "revision": "c28da54eb9a8e33b4fbb",
+    "url": "/task-mcm/static/js/runtime-main.dc99b010.js"
+  }
+]);
